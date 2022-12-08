d5e1c1eaada9aca93cbc4f655f4bf53e26a884ec
sahil4683 <sahil4683@gmail.com>
2022-12-08 23:54:29 +0545

    vs commit

---
+++ src/main/java/com/sp/asset_monitoring/AssetMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.sp.asset_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sp/asset_monitoring/AssetMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.asset_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
