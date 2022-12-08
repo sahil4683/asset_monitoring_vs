7f878e2541c66e9e9d6798129040e6fcd5bf40b4
Sahil Patil <43109674+sahil4683@users.noreply.github.com>
2022-12-08 23:58:50 +0545

    wrong c

--- src/main/java/com/sp/asset_monitoring/AssetMonitoringApplication.java
+++ src/main/java/com/sp/asset_monitoring/AssetMonitoringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AssetMonitoringApplication {
 
 	public static void main(String[] args) {
+		System.out("wells")
 		SpringApplication.run(AssetMonitoringApplication.class, args);
 	}
 
